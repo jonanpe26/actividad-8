fbdefc11f5a51bcce12cb5cc32e83fdef05295b3
jonan <heber2025sandoval@gmail.com>
2025-07-22 00:16:34 -0600

    4to

--- actividad 8.py
+++ actividad 8.py
@@ -28,4 +28,54 @@ def contar(palabra,letra):
 def invertir(cadena):
     if cadena =="":
         return ""
-    return contar()
+    return invertir(cadena)+cadena[0]
+def potencia(base, exponente):
+    if exponente==0:
+        return 1
+    else:
+        return base*potencia(base, exponente-1)
+
+while True:
+    menu()
+    opcion=input("selecciona la opcion")
+    if opcion =="1":
+        num=input("ingrese un numero entero positivo")
+        if num.isdigit():
+            print("factorial",factorial(int(num)))
+        else:
+            print("error")
+    elif opcion == "2":
+        n =input("ingrese un numero entero positivo")
+        if n.isdigit():
+            print("suma de primeros", n, "numeros", suma(int(n)))
+        else:
+            print("error")
+    elif opcion == "3":
+        n=input("ingrese la posicion de fibonacci")
+        if n.isdigit():
+            print(f"fibonacci({n} =", fibonacci(int(n)))
+        else:
+            print("error")
+    elif opcion == "4":
+        palabra =input("ingrese la palabra")
+        letra = input("ingrese la letra a contar")
+        if (letra)==1:
+            print(f"la letra{letra} aparece {contar(palabra, letra)} veces")
+        else:
+            print("error")
+    elif opcion == "5":
+        cadena = input("ingrese el texto")
+        print("cadena invertida", invertir(cadena))
+    elif opcion == "6":
+        base = int(input("Base: "))
+        exp = int(input("Exponente: "))
+        if exp >= 0:
+            print(f"{base}^{exp} = {potencia(base, exp)}")
+        else:
+            print("Error")
+    elif opcion == "7":
+        print("¡Gracias por usar el programa!")
+        break
+    else:
+        print("Opción inválida. Intente nuevamente.")
+
