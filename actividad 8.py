2f7ba387328bac5081741c7b9382a4558a43350d
jonan <heber2025sandoval@gmail.com>
2025-07-21 19:48:06 -0600

    2do

--- actividad 8.py
+++ actividad 8.py
@@ -1,1 +1,23 @@
-def menu()
+def menu():
+    print("1.Calcular el facorial de un numero")
+    print("2.Suma de los primeros N numeros naturales")
+    print("3. Calcular el e-nesimo numero fibonacci")
+    print("4.Contar cuatas veces aparece una letra en una palabra")
+    print("5.Invertir una cadena de texto")
+    print("6.Calcukar la potencia de un numero")
+    print("7.salir")
+
+def factorial(n):
+    if n==0:
+        return 1
+    return n*factorial(n-1)
+def suma(n):
+    if n==1:
+        return 1
+    return n+suma(n-1)
+def fibonacci(n):
+    if n ==0:
+        return 0
+    if n==1:
+        return 1
+    return fibonacci(n-1)+fibonacci(n-2)
